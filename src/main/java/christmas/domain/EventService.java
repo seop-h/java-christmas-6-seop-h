6deee3015dbe7d0d359973907c4300f66272538a
황중섭 <jshwang9709@gmail.com>
2023-11-11 22:13:46 +0900

    feat(domain): 주말 할인 적용 기능 추가

--- src/main/java/christmas/domain/EventService.java
+++ src/main/java/christmas/domain/EventService.java
@@ -14,7 +14,9 @@ import static christmas.domain.constant.EventAmountConst.D_DAY_START;
 import static christmas.domain.constant.EventAmountConst.MENU_DISCOUNT;
 import static christmas.domain.model.Event.CHRISTMAS_D_DAY;
 import static christmas.domain.model.Event.WEEKDAY;
+import static christmas.domain.model.Event.WEEKEND;
 import static christmas.domain.model.menu.Type.DESSERT;
+import static christmas.domain.model.menu.Type.MAIN;
 
 
 public class EventService {
@@ -37,11 +39,20 @@ public class EventService {
         eventDetails.put(CHRISTMAS_D_DAY, D_DAY_START + date * D_DAY_INCREMENT);
     }
 
-    private void applyWeekday() {
+    private void applyEitherOne() {
         if (reservation.getDate().isWeekend()){
+            applyWeekend();
             return;
         }
+        applyWeekday();
+    }
 
+    private void applyWeekend() {
+        int dish = reservation.calculateDishesOf(MAIN);
+        eventDetails.put(WEEKEND, dish * MENU_DISCOUNT);
+    }
+
+    private void applyWeekday() {
         int dish = reservation.calculateDishesOf(DESSERT);
         eventDetails.put(WEEKDAY, dish * MENU_DISCOUNT);
     }

044fd682be25ce7017bfe12d81dff68f3bfa4b0d
황중섭 <jshwang9709@gmail.com>
2023-11-13 23:19:25 +0900

    feat(ui): 주문 메뉴와 개수 입력 기능 추가

--- src/main/java/christmas/ui/input/InputView.java
+++ src/main/java/christmas/ui/input/InputView.java
@@ -3,11 +3,18 @@ package christmas.ui.input;
 import camp.nextstep.edu.missionutils.Console;
 
 import static christmas.ui.input.InputMessage.EXPECTED_VISIT_DATE;
+import static christmas.ui.input.InputMessage.ORDER_MENU_AND_QUANTITY;
 
 public class InputView {
 
     public static String readDate() {
         System.out.println(EXPECTED_VISIT_DATE);
+        String input = Console.readLine();
+        return input;
+    }
+
+    public static String readMenu() {
+        System.out.println(ORDER_MENU_AND_QUANTITY);
         String input = Console.readLine();
         return input;
     }

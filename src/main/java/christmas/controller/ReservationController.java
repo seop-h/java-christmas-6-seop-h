7abb8f9665afd8516f94a40e8df0875925aa0642
황중섭 <jshwang9709@gmail.com>
2023-11-14 02:09:58 +0900

    feat(controller): 입력받은 주문정보를 map으로 변환하는 기능 추가

--- src/main/java/christmas/controller/ReservationController.java
+++ src/main/java/christmas/controller/ReservationController.java
@@ -3,11 +3,18 @@ package christmas.controller;
 import christmas.controller.util.Conversion;
 import christmas.ui.input.view.InputView;
 
+import java.util.Map;
+
 public class ReservationController {
 
     public int initDate() {
         String date = InputView.readDate();
         return Conversion.toInt(date);
+    }
+
+    public Map<String, Integer> initOrders() {
+        String orders = InputView.readMenu();
+        return Conversion.toMap(orders);
     }
 
 }
--- src/main/java/christmas/controller/util/Conversion.java
+++ src/main/java/christmas/controller/util/Conversion.java
@@ -1,6 +1,11 @@
 package christmas.controller.util;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import static christmas.constant.ErrorMessage.NOT_INVALID_DATE;
+import static christmas.constant.ErrorMessage.NOT_INVALID_ORDER;
 
 public class Conversion {
 
@@ -11,6 +16,32 @@ public class Conversion {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NOT_INVALID_DATE, e);
         }
+    }
+
+    public static Map<String, Integer> toMap(String input) {
+        Map<String, Integer> result = new LinkedHashMap<>();
+
+        try {
+            storeEachElementInMap(input, result);
+            return result;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NOT_INVALID_ORDER, e);
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
+    }
+
+    private static void storeEachElementInMap(String input, Map<String, Integer> result) {
+        Arrays.stream(input.split(","))
+                .map(menuAndServing -> menuAndServing.split("-"))
+                .peek(anOrder -> {
+                    if (anOrder.length != 2 || result.containsKey(anOrder[0])) {
+                        throw new IllegalArgumentException(NOT_INVALID_ORDER);
+                    }
+                })
+                .forEach(anOrder ->
+                        result.put(anOrder[0], Integer.parseInt(anOrder[1]))
+                );
     }
 
 }

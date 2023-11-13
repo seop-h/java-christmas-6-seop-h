e8c76579889fea5c71d35ac722b3b8bb9f3ede1f
황중섭 <jshwang9709@gmail.com>
2023-11-13 15:35:22 +0900

    test(domain): 각 이벤트마다 할인 금액이 정상적으로 계산되는지 테스트

---
+++ src/test/java/christmas/domain/service/event/EventTest.java
@@ -0,0 +1,51 @@
+package christmas.domain.service.event;
+
+import christmas.domain.model.order.menu.Menu;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static christmas.domain.service.event.Event.CHRISTMAS_D_DAY;
+import static christmas.domain.service.event.Event.GIVEAWAY;
+import static christmas.domain.service.event.Event.SPECIAL;
+import static christmas.domain.service.event.Event.WEEKDAY;
+import static christmas.domain.service.event.Event.WEEKEND;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:1000", "2:1100", "3:1200", "24:3300", "25:3400"}, delimiter = ':')
+    @DisplayName("크리스마스 디데이 할인금액 계산이 정상적으로 적용되는지 확인한다.")
+    void checkChristmasDDayDiscount(Integer date, Integer expected) {
+        assertThat(CHRISTMAS_D_DAY.calculateBenefitAmount(date)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:0", "1:2023", "2:4046", "3:6069", "20:40460"}, delimiter = ':')
+    @DisplayName("평일 할인금액 계산이 정상적으로 적용되는지 확인한다.")
+    void checkWeekdayDiscount(Integer servings, Integer expected) {
+        assertThat(WEEKDAY.calculateBenefitAmount(servings)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:0", "1:2023", "2:4046", "3:6069", "20:40460"}, delimiter = ':')
+    @DisplayName("주말 할인금액 계산이 정상적으로 적용되는지 확인한다.")
+    void checkWeekendDiscount(Integer servings, Integer expected) {
+        assertThat(WEEKEND.calculateBenefitAmount(servings)).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("특별 할인금액 계산이 정상적으로 적용되는지 확인한다.")
+    void checkSpecialDiscount() {
+        assertThat(SPECIAL.calculateBenefitAmount(null)).isEqualTo(1_000);
+    }
+
+    @Test
+    @DisplayName("증정 이벤트의 값어치 계산이 정상적으로 적용되는지 확인한다.")
+    void checkGiveawayWorth() {
+        assertThat(GIVEAWAY.calculateBenefitAmount(null)).isEqualTo(Menu.CHAMPAGNE.getPrice());
+    }
+
+}
